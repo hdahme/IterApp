b797e55036531cb4e42988642fbd3597b4b40b0b
Harrison Dahme <harrison.dahme@gmail.com>
2014-04-10 16:22:27 -0700

    Working maps demo, placing custom marker, plus data model of a location update

---
+++ src/com/example/mapdemo/models/Location.java
@@ -0,0 +1,33 @@
+package com.example.mapdemo.models;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+	
+	private double lat;
+	private double lng;
+	private int timestamp;
+	private int userId;
+	private int eventId;
+	
+	public Location(double lat, double lng) {
+		this.lat = lat;
+		this.lng = lng;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public int getTimestamp() {
+		return timestamp;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public int getEventId() {
+		return eventId;
+	}
+
+}
