fbee6dde7c966f58dddb9dd2a85a33640e23b89e
Matt Johnson <mrjz@umich.edu>
2014-04-10 23:32:54 -0700

    added model class for Event

---
+++ src/com/example/mapdemo/models/Event.java
@@ -0,0 +1,52 @@
+package com.example.mapdemo.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Event")
+public class Event extends ParseObject {
+    public Event() {
+        super();
+    }
+
+    public boolean isActive() {
+        return getBoolean("active");
+    }
+
+    public void setActive(boolean active) {
+        put("active", active);
+    }
+
+    public String getType() {
+        return getString("type");
+    }
+
+    public void setType(String type) {
+        put("type", type);
+    }
+
+    public ParseUser getOwner() {
+        return getParseUser("owner");
+    }
+
+    public void setOwner(ParseUser user) {
+        put("owner", user);
+    }
+
+    public String getTitle() {
+        return getString("title");
+    }
+
+    public void setTitle(String title) {
+        put("title", title);
+    }
+
+    public String getDescription() {
+        return getString("description");
+    }
+
+    public void setDescription(String description) {
+        put("description", description);
+    }
+}
