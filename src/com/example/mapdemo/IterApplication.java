71bc66693e5a298ef6aa23b3a261a7cad824b97e
Matt Johnson <mrjz@umich.edu>
2014-04-10 23:34:35 -0700

    added IterApplication class that takes care of Parse initialization in onCreate()

---
+++ src/com/example/mapdemo/IterApplication.java
@@ -0,0 +1,30 @@
+package com.example.mapdemo;
+
+import android.app.Application;
+
+import com.example.mapdemo.models.Event;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class IterApplication extends Application {
+    private static final String APPLICATION_ID = "3BFzwUwXukcS8oIiNwiUwCyHtxVkaxQoXFrjZl9m";
+    private static final String CLIENT_KEY = "5cg4hpkHCHECqHfbIBwBItQvZjUPoxzmwBgmPQk1";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		ParseObject.registerSubclass(Event.class);
+		Parse.initialize(this, IterApplication.APPLICATION_ID, IterApplication.CLIENT_KEY);
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+
+		ParseACL.setDefaultACL(defaultACL, true);
+	}
+}
